b392f5b1eaae7b96db222512df1ef30494268afa
Conor <70652079+conorvenus@users.noreply.github.com>
2024-10-04 14:08:20 +0000

    Added new headers, and added a skill section

--- src/lib/components/carousel.svelte
+++ src/lib/components/carousel.svelte
@@ -3,25 +3,41 @@
         alt: string,
         src: string
     }[];
+
+    export let title: string;
 </script>
 
-<div class="flex flex-col gap-4 bg-content/5 p-8 rounded-md">
-    <div class="parent cursor-pointer w-full overflow-hidden flex gap-8">
-        {#each [...Array(10)] as _}
-            <div class="images flex gap-8">
-                {#each images as image}
-                    <div class="image-container">
-                        <img src={image.src} alt={image.alt} title={image.alt} />
-                    </div>
-                {/each}
-            </div>
-        {/each}
+<div class="flex flex-col gap-4">
+    <h3 class="title bg-gradient-to-r from-primary to-secondary rounded-lg text-base font-semibold text-2xl w-fit px-8 py-4">{title}</h3>
+    <div class="flex flex-col gap-4 bg-content/5 p-8 rounded-md relative">
+        <div class="parent cursor-pointer w-full overflow-hidden flex gap-8">
+            {#each [...Array(10)] as _}
+                <div class="images flex gap-8">
+                    {#each images as image}
+                        <div class="image-container">
+                            <img src={image.src} alt={image.alt} title={image.alt} />
+                        </div>
+                    {/each}
+                </div>
+            {/each}
+        </div>
     </div>
 </div>
 
 <style lang="postcss">
+    .title {
+        @apply relative;
+    }
+
+    .title::after {
+        content: '';
+        @apply absolute bg-gradient-to-r from-secondary to-primary box-content w-full h-full top-0 left-0 blur-xl -z-10 opacity-25;
+    }
+    
     .image-container img {
         min-width: 100px;
+        aspect-ratio: 1/1;
+        object-fit: contain;
     }
 
     .image-container {
--- src/lib/components/skills.svelte
+++ src/lib/components/skills.svelte
@@ -2,9 +2,9 @@
     import Carousel from "./carousel.svelte"
 </script>
 
-<section class="min-h-screen py-32 flex flex-col gap-12" id="skills">
+<section class="min-h-screen py-32 flex flex-col gap-16" id="skills">
     <h2 class="text-3xl font-bold text-content">Skills</h2>
-    <Carousel images={[
+    <Carousel title="Languages" images={[
         {
             alt: 'html',
             src: 'https://user-images.githubusercontent.com/25181517/192158954-f88b5814-d510-4564-b285-dff7d6400dad.png'
@@ -33,13 +33,59 @@
             alt: 'python',
             src: 'https://user-images.githubusercontent.com/25181517/183423507-c056a6f9-1ba8-4312-a350-19bcbc5a8697.png'
         },
+        // {
+        //     alt: 'rust'
+        //     src: 'https://user-images.githubusercontent.com/25181517/192599922-3a8ceb1c-ff1d-40bc-b73c-99ea1182d8ad.png'
+        // },
+        // {
+        //     alt: 'go',
+        //     src: 'https://user-images.githubusercontent.com/25181517/192149581-88194d20-1a37-4be8-8801-5dc0017ffbbe.png'
+        // }
+    ]} />
+    <Carousel title="Technologies" images={[
         {
-            alt: 'rust',
-            src: 'https://user-images.githubusercontent.com/25181517/192599922-3a8ceb1c-ff1d-40bc-b73c-99ea1182d8ad.png'
+            alt: 'react',
+            src: 'https://user-images.githubusercontent.com/25181517/183897015-94a058a6-b86e-4e42-a37f-bf92061753e5.png'
         },
         {
-            alt: 'go',
-            src: 'https://user-images.githubusercontent.com/25181517/192149581-88194d20-1a37-4be8-8801-5dc0017ffbbe.png'
+            alt: 'svelte',
+            src: 'https://raw.githubusercontent.com/sveltejs/svelte/29052aba7d0b78316d3a52aef1d7ddd54fe6ca84/site/static/images/svelte-android-chrome-512.png'
+        },
+        {
+            alt: 'nestjs',
+            src: 'https://static-00.iconduck.com/assets.00/nestjs-icon-2048x2040-3rrvcej8.png'
+        },
+        {
+            alt: 'django',
+            src: 'https://static-00.iconduck.com/assets.00/django-icon-1606x2048-lwmw1z73.png'
+        },
+        {
+            alt: 'sql',
+            src: 'https://static-00.iconduck.com/assets.00/sql-database-generic-icon-1521x2048-d0vdpxpg.png'
+        },
+        {
+            alt: 'nginx',
+            src: 'https://user-images.githubusercontent.com/25181517/183345125-9a7cd2e6-6ad6-436f-8490-44c903bef84c.png'
+        },
+        {
+            alt: 'tailwindcss',
+            src: 'https://user-images.githubusercontent.com/25181517/202896760-337261ed-ee92-4979-84c4-d4b829c7355d.png'
+        },
+        {
+            alt: 'git',
+            src: 'https://git-scm.com/images/logos/downloads/Git-Icon-1788C.png'
+        },
+        {
+            alt: 'github',
+            src: 'https://cdn-icons-png.flaticon.com/512/25/25231.png'
+        },
+        {
+            alt: 'vue',
+            src: 'https://user-images.githubusercontent.com/25181517/117448124-a2da9800-af3e-11eb-85d2-bd1b69b65603.png'
+        },
+        {
+            alt: 'pytorch',
+            src: 'https://cdn-images-1.medium.com/max/1200/1*KKADWARPMxHb-WMxCgW_xA.png'
         }
     ]} />
 </section>

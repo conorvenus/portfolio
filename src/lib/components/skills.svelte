6cfb635fadee354de94684594cb82234234fc8ff
Conor <70652079+conorvenus@users.noreply.github.com>
2024-10-04 13:17:30 +0000

    Added skills section

--- src/lib/components/skills.svelte
+++ src/lib/components/skills.svelte
@@ -1,70 +1,192 @@
-<section class="min-h-screen py-32" id="skills">
-    <h2 class="text-3xl font-bold mb-8 text-content">Skills</h2>
-    <div class="flex gap-10 flex-wrap justify-center mt-16">
-        <div class="image-container">
-            <img src="https://user-images.githubusercontent.com/25181517/192158954-f88b5814-d510-4564-b285-dff7d6400dad.png" alt="HTML" title="HTML"/>
+<section class="min-h-screen py-32 flex flex-col gap-12" id="skills">
+    <h2 class="text-3xl font-bold text-content">Skills</h2>
+    <div class="flex flex-col gap-4 bg-content/5 p-8 rounded-md">
+        <div class="w-full overflow-hidden flex gap-8">
+            <div class="images flex gap-8">
+                <div class="image-container">
+                    <img src="https://user-images.githubusercontent.com/25181517/192158954-f88b5814-d510-4564-b285-dff7d6400dad.png" alt="HTML" title="HTML"/>
+                </div>
+                <div class="image-container">
+                    <img src="https://user-images.githubusercontent.com/25181517/183898674-75a4a1b1-f960-4ea9-abcb-637170a00a75.png" alt="CSS" title="CSS"/>
+                </div>
+                <div class="image-container">
+                    <img  src="https://user-images.githubusercontent.com/25181517/121405384-444d7300-c95d-11eb-959f-913020d3bf90.png" alt="C#" title="C#"/>
+                </div>
+                <div class="image-container">
+                    <img  src="https://user-images.githubusercontent.com/25181517/183423507-c056a6f9-1ba8-4312-a350-19bcbc5a8697.png" alt="Python" title="Python"/>
+                </div>
+                <div class="image-container">
+                    <img  src="https://user-images.githubusercontent.com/25181517/117447155-6a868a00-af3d-11eb-9cfe-245df15c9f3f.png" alt="JavaScript" title="JavaScript"/>
+                </div>
+                <div class="image-container">
+                    <img  src="https://user-images.githubusercontent.com/25181517/183890598-19a0ac2d-e88a-4005-a8df-1ee36782fde1.png" alt="TypeScript" title="TypeScript"/>
+                </div>
+                <div class="image-container">
+                    <img  src="https://user-images.githubusercontent.com/25181517/183568594-85e280a7-0d7e-4d1a-9028-c8c2209e073c.png" alt="Node.js" title="Node.js"/>
+                </div>
+                <div class="image-container">
+                    <img  src="https://user-images.githubusercontent.com/25181517/117201156-9a724800-adec-11eb-9a9d-3cd0f67da4bc.png" alt="Java" title="Java"/>
+                </div>
+            </div>
+            <div class="images flex gap-8">
+                <div class="image-container">
+                    <img src="https://user-images.githubusercontent.com/25181517/192158954-f88b5814-d510-4564-b285-dff7d6400dad.png" alt="HTML" title="HTML"/>
+                </div>
+                <div class="image-container">
+                    <img src="https://user-images.githubusercontent.com/25181517/183898674-75a4a1b1-f960-4ea9-abcb-637170a00a75.png" alt="CSS" title="CSS"/>
+                </div>
+                <div class="image-container">
+                    <img  src="https://user-images.githubusercontent.com/25181517/121405384-444d7300-c95d-11eb-959f-913020d3bf90.png" alt="C#" title="C#"/>
+                </div>
+                <div class="image-container">
+                    <img  src="https://user-images.githubusercontent.com/25181517/183423507-c056a6f9-1ba8-4312-a350-19bcbc5a8697.png" alt="Python" title="Python"/>
+                </div>
+                <div class="image-container">
+                    <img  src="https://user-images.githubusercontent.com/25181517/117447155-6a868a00-af3d-11eb-9cfe-245df15c9f3f.png" alt="JavaScript" title="JavaScript"/>
+                </div>
+                <div class="image-container">
+                    <img  src="https://user-images.githubusercontent.com/25181517/183890598-19a0ac2d-e88a-4005-a8df-1ee36782fde1.png" alt="TypeScript" title="TypeScript"/>
+                </div>
+                <div class="image-container">
+                    <img  src="https://user-images.githubusercontent.com/25181517/183568594-85e280a7-0d7e-4d1a-9028-c8c2209e073c.png" alt="Node.js" title="Node.js"/>
+                </div>
+                <div class="image-container">
+                    <img  src="https://user-images.githubusercontent.com/25181517/117201156-9a724800-adec-11eb-9a9d-3cd0f67da4bc.png" alt="Java" title="Java"/>
+                </div>
+            </div>
         </div>
-        <div class="image-container">
-            <img src="https://user-images.githubusercontent.com/25181517/183898674-75a4a1b1-f960-4ea9-abcb-637170a00a75.png" alt="CSS" title="CSS"/>
-        </div>
-        <div class="image-container">
-            <img  src="https://user-images.githubusercontent.com/25181517/202896760-337261ed-ee92-4979-84c4-d4b829c7355d.png" alt="Tailwind CSS" title="Tailwind CSS"/>
-        </div>
-        <div class="image-container">
-            <img  src="https://user-images.githubusercontent.com/25181517/121405384-444d7300-c95d-11eb-959f-913020d3bf90.png" alt="C#" title="C#"/>
-        </div>
-        <div class="image-container">
-            <img  src="https://user-images.githubusercontent.com/25181517/183423507-c056a6f9-1ba8-4312-a350-19bcbc5a8697.png" alt="Python" title="Python"/>
-        </div>
-        <div class="image-container">
-            <img  src="https://user-images.githubusercontent.com/25181517/117447155-6a868a00-af3d-11eb-9cfe-245df15c9f3f.png" alt="JavaScript" title="JavaScript"/>
-        </div>
-        <div class="image-container">
-            <img  src="https://user-images.githubusercontent.com/25181517/183897015-94a058a6-b86e-4e42-a37f-bf92061753e5.png" alt="React" title="React"/>
-        </div>
-        <div class="image-container">
-            <img  src="https://user-images.githubusercontent.com/25181517/183890598-19a0ac2d-e88a-4005-a8df-1ee36782fde1.png" alt="TypeScript" title="TypeScript"/>
-        </div>
-        <div class="image-container">
-            <img  src="https://user-images.githubusercontent.com/25181517/183568594-85e280a7-0d7e-4d1a-9028-c8c2209e073c.png" alt="Node.js" title="Node.js"/>
-        </div>
-        <div class="image-container">
-            <img  src="https://user-images.githubusercontent.com/25181517/183859966-a3462d8d-1bc7-4880-b353-e2cbed900ed6.png" alt="Express" title="Express"/>
-        </div>
-        <div class="image-container">
-            <img  src="https://user-images.githubusercontent.com/25181517/117201156-9a724800-adec-11eb-9a9d-3cd0f67da4bc.png" alt="Java" title="Java"/>
-        </div>
-        <div class="image-container">
-            <img  src="https://user-images.githubusercontent.com/25181517/121405754-b4f48f80-c95d-11eb-8893-fc325bde617f.png" alt=".NET Core" title=".NET Core"/>
-        </div>
-        <div class="image-container">
-            <img  src="https://github.com/marwin1991/profile-technology-icons/assets/136815194/519bfaf3-c242-431e-a269-876979f05574" alt="Nest.js" title="Nest.js"/>
-        </div>
-        <div class="image-container">
-            <img  src="https://github.com/marwin1991/profile-technology-icons/assets/136815194/e56b5093-2f58-40cc-b194-5bdde41077b5" alt="Svelte" title="Svelte"/>
-        </div>
-        <div class="image-container">
-            <img  src="https://user-images.githubusercontent.com/25181517/183423775-2276e25d-d43d-4e58-890b-edbc88e915f7.png" alt="Flask" title="Flask"/>
-        </div>
-        <div class="image-container">
-            <img  src="https://github.com/marwin1991/profile-technology-icons/assets/62091613/9bf5650b-e534-4eae-8a26-8379d076f3b4" alt="Django" title="Django"/>
-        </div>
-        <div class="image-container">
-            <img  src="https://user-images.githubusercontent.com/25181517/183896132-54262f2e-6d98-41e3-8888-e40ab5a17326.png" alt="AWS" title="AWS"/>
+    </div>
+    <div class="flex flex-col gap-4 bg-content/5 p-8 rounded-md">
+        <div class="w-full overflow-hidden flex gap-8">
+            <div class="images flex gap-8">
+                <div class="image-container">
+                    <img src="https://user-images.githubusercontent.com/25181517/192158954-f88b5814-d510-4564-b285-dff7d6400dad.png" alt="HTML" title="HTML"/>
+                </div>
+                <div class="image-container">
+                    <img src="https://user-images.githubusercontent.com/25181517/183898674-75a4a1b1-f960-4ea9-abcb-637170a00a75.png" alt="CSS" title="CSS"/>
+                </div>
+                <div class="image-container">
+                    <img  src="https://user-images.githubusercontent.com/25181517/202896760-337261ed-ee92-4979-84c4-d4b829c7355d.png" alt="Tailwind CSS" title="Tailwind CSS"/>
+                </div>
+                <div class="image-container">
+                    <img  src="https://user-images.githubusercontent.com/25181517/121405384-444d7300-c95d-11eb-959f-913020d3bf90.png" alt="C#" title="C#"/>
+                </div>
+                <div class="image-container">
+                    <img  src="https://user-images.githubusercontent.com/25181517/183423507-c056a6f9-1ba8-4312-a350-19bcbc5a8697.png" alt="Python" title="Python"/>
+                </div>
+                <div class="image-container">
+                    <img  src="https://user-images.githubusercontent.com/25181517/117447155-6a868a00-af3d-11eb-9cfe-245df15c9f3f.png" alt="JavaScript" title="JavaScript"/>
+                </div>
+                <div class="image-container">
+                    <img  src="https://user-images.githubusercontent.com/25181517/183897015-94a058a6-b86e-4e42-a37f-bf92061753e5.png" alt="React" title="React"/>
+                </div>
+                <div class="image-container">
+                    <img  src="https://user-images.githubusercontent.com/25181517/183890598-19a0ac2d-e88a-4005-a8df-1ee36782fde1.png" alt="TypeScript" title="TypeScript"/>
+                </div>
+                <div class="image-container">
+                    <img  src="https://user-images.githubusercontent.com/25181517/183568594-85e280a7-0d7e-4d1a-9028-c8c2209e073c.png" alt="Node.js" title="Node.js"/>
+                </div>
+                <div class="image-container">
+                    <img  src="https://user-images.githubusercontent.com/25181517/183859966-a3462d8d-1bc7-4880-b353-e2cbed900ed6.png" alt="Express" title="Express"/>
+                </div>
+                <div class="image-container">
+                    <img  src="https://user-images.githubusercontent.com/25181517/117201156-9a724800-adec-11eb-9a9d-3cd0f67da4bc.png" alt="Java" title="Java"/>
+                </div>
+                <div class="image-container">
+                    <img  src="https://user-images.githubusercontent.com/25181517/121405754-b4f48f80-c95d-11eb-8893-fc325bde617f.png" alt=".NET Core" title=".NET Core"/>
+                </div>
+                <div class="image-container">
+                    <img  src="https://github.com/marwin1991/profile-technology-icons/assets/136815194/519bfaf3-c242-431e-a269-876979f05574" alt="Nest.js" title="Nest.js"/>
+                </div>
+                <div class="image-container">
+                    <img  src="https://github.com/marwin1991/profile-technology-icons/assets/136815194/e56b5093-2f58-40cc-b194-5bdde41077b5" alt="Svelte" title="Svelte"/>
+                </div>
+                <div class="image-container">
+                    <img  src="https://user-images.githubusercontent.com/25181517/183423775-2276e25d-d43d-4e58-890b-edbc88e915f7.png" alt="Flask" title="Flask"/>
+                </div>
+                <div class="image-container">
+                    <img  src="https://github.com/marwin1991/profile-technology-icons/assets/62091613/9bf5650b-e534-4eae-8a26-8379d076f3b4" alt="Django" title="Django"/>
+                </div>
+                <div class="image-container">
+                    <img  src="https://user-images.githubusercontent.com/25181517/183896132-54262f2e-6d98-41e3-8888-e40ab5a17326.png" alt="AWS" title="AWS"/>
+                </div>
+            </div>
+            <div class="images flex gap-8">
+                <div class="image-container">
+                    <img src="https://user-images.githubusercontent.com/25181517/192158954-f88b5814-d510-4564-b285-dff7d6400dad.png" alt="HTML" title="HTML"/>
+                </div>
+                <div class="image-container">
+                    <img src="https://user-images.githubusercontent.com/25181517/183898674-75a4a1b1-f960-4ea9-abcb-637170a00a75.png" alt="CSS" title="CSS"/>
+                </div>
+                <div class="image-container">
+                    <img  src="https://user-images.githubusercontent.com/25181517/202896760-337261ed-ee92-4979-84c4-d4b829c7355d.png" alt="Tailwind CSS" title="Tailwind CSS"/>
+                </div>
+                <div class="image-container">
+                    <img  src="https://user-images.githubusercontent.com/25181517/121405384-444d7300-c95d-11eb-959f-913020d3bf90.png" alt="C#" title="C#"/>
+                </div>
+                <div class="image-container">
+                    <img  src="https://user-images.githubusercontent.com/25181517/183423507-c056a6f9-1ba8-4312-a350-19bcbc5a8697.png" alt="Python" title="Python"/>
+                </div>
+                <div class="image-container">
+                    <img  src="https://user-images.githubusercontent.com/25181517/117447155-6a868a00-af3d-11eb-9cfe-245df15c9f3f.png" alt="JavaScript" title="JavaScript"/>
+                </div>
+                <div class="image-container">
+                    <img  src="https://user-images.githubusercontent.com/25181517/183897015-94a058a6-b86e-4e42-a37f-bf92061753e5.png" alt="React" title="React"/>
+                </div>
+                <div class="image-container">
+                    <img  src="https://user-images.githubusercontent.com/25181517/183890598-19a0ac2d-e88a-4005-a8df-1ee36782fde1.png" alt="TypeScript" title="TypeScript"/>
+                </div>
+                <div class="image-container">
+                    <img  src="https://user-images.githubusercontent.com/25181517/183568594-85e280a7-0d7e-4d1a-9028-c8c2209e073c.png" alt="Node.js" title="Node.js"/>
+                </div>
+                <div class="image-container">
+                    <img  src="https://user-images.githubusercontent.com/25181517/183859966-a3462d8d-1bc7-4880-b353-e2cbed900ed6.png" alt="Express" title="Express"/>
+                </div>
+                <div class="image-container">
+                    <img  src="https://user-images.githubusercontent.com/25181517/117201156-9a724800-adec-11eb-9a9d-3cd0f67da4bc.png" alt="Java" title="Java"/>
+                </div>
+                <div class="image-container">
+                    <img  src="https://user-images.githubusercontent.com/25181517/121405754-b4f48f80-c95d-11eb-8893-fc325bde617f.png" alt=".NET Core" title=".NET Core"/>
+                </div>
+                <div class="image-container">
+                    <img  src="https://github.com/marwin1991/profile-technology-icons/assets/136815194/519bfaf3-c242-431e-a269-876979f05574" alt="Nest.js" title="Nest.js"/>
+                </div>
+                <div class="image-container">
+                    <img  src="https://github.com/marwin1991/profile-technology-icons/assets/136815194/e56b5093-2f58-40cc-b194-5bdde41077b5" alt="Svelte" title="Svelte"/>
+                </div>
+                <div class="image-container">
+                    <img  src="https://user-images.githubusercontent.com/25181517/183423775-2276e25d-d43d-4e58-890b-edbc88e915f7.png" alt="Flask" title="Flask"/>
+                </div>
+                <div class="image-container">
+                    <img  src="https://github.com/marwin1991/profile-technology-icons/assets/62091613/9bf5650b-e534-4eae-8a26-8379d076f3b4" alt="Django" title="Django"/>
+                </div>
+                <div class="image-container">
+                    <img  src="https://user-images.githubusercontent.com/25181517/183896132-54262f2e-6d98-41e3-8888-e40ab5a17326.png" alt="AWS" title="AWS"/>
+                </div>
+            </div>
         </div>
     </div>
 </section>
 
 <style lang="postcss">
-    .image-container {
-        width: 100px;
-        position: relative;
+    .image-container img {
+        min-width: 100px;
     }
 
-    .image-container::before {
-        content: '';
-        position: absolute;
-        inset: 0;
-        @apply bg-gradient-to-r from-primary to-secondary rounded-full blur-xl -z-10 opacity-40;
+    .image-container {
+        @apply bg-content/10 p-8 rounded-lg relative;
+    }
+
+    .images {
+        animation: scroll 30s 0.5s linear infinite;
+    }
+
+    @keyframes  scroll {
+        from {
+            transform: translateX(0);
+        }
+        to {
+            transform: translateX(-100%);
+        }
     }
 </style>
